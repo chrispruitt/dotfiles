bbbdf2ac81031f029debf711647f3765227b2b95
chrispruitt <chris.pruitt15@gmail.com>
2019-04-22 10:06:37 -0500

    bug fix on dns config

--- DNS/dns.functions.zsh
+++ DNS/dns.functions.zsh
@@ -1,10 +1,10 @@
 #Add the g2l-office.txt file with a list of ips for your dns config
 
 function dns-setup-g2l-office() {
- local IPS=$(<~/.dotfiles/DNS/g2l-office.txt)
- IPS=$(echo ${value//$'\n'/ })
-
+ local value=$(<~/.dotfiles/DNS/g2l-office.txt)
+ local IPS=${value//$'\n'/ }
  local DNS_COMMAND="networksetup -setdnsservers Wi-Fi ${IPS}"
+
  echo ${DNS_COMMAND}
  eval ${DNS_COMMAND}
 }
