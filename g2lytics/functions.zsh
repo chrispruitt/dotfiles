725ecdc272a9e0b7368ce02b29de95dc039a4af6
chrispruitt <chris.pruitt15@gmail.com>
2019-05-22 13:19:53 -0500

    add copy db from rds

--- g2lytics/functions.zsh
+++ g2lytics/functions.zsh
@@ -27,3 +27,41 @@ function db-login() {
 
   PGHOST=${DB_HOST} PGUSER=${DB_USER} PGPASSWORD=${DB_PASSWORD} psql -d ${DB_NAME}
 }
+
+function db-copy-remote() {
+  local env=$1
+  local service=$2
+
+  local result=$(get-ssm-params -path /${env}/${service} | jq)
+
+  local DB_HOST=$(echo ${result} | jq -r .DB_HOST)
+  local DB_USER=$(echo ${result} | jq -r .DB_USER)
+  local DB_PASSWORD=$(echo ${result} | jq -r .DB_PASSWORD)
+  local DB_PORT=$(echo ${result} | jq -r .DB_PORT)
+  local DB_NAME=$(echo ${result} | jq -r .DB_NAME)
+
+  if [[ "${DB_PORT}" = "null" ]]
+  then
+        DB_PORT=5432
+  fi
+
+  if [[ "${DB_NAME}" = "null" ]]
+  then
+        DB_NAME=$(echo ${service} | tr - _)
+  fi
+
+  echo "Creating Local Database: ${DB_NAME}"
+  psql postgres -c "CREATE DATABASE ${DB_NAME}"
+
+  DUMP_FILE=./${env}_${DB_NAME}.dump
+
+  echo "Copying Database ${DB_NAME} from ${env}..."
+  PGPASSWORD=${DB_PASSWORD} pg_dump -Fc -v -h ${DB_HOST} -U ${DB_USER} ${DB_NAME} > ${DUMP_FILE}
+
+  pg_restore -v -h localhost -U $(whoami) -d ${DB_NAME} --no-acl ${DUMP_FILE}
+
+#  echo "\n Dump file saved to ${DUMP_FILE}"
+  rm ${DUMP_FILE}
+
+  echo "Done."
+}
