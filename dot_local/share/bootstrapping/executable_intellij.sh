7aa8f7ce1808033f063bfd49ae33f36f21121d39
Chris Pruitt <chris.pruitt15@gmail.com>
2022-10-28 07:59:45 -0400

    save intellij progress

---
+++ dot_local/share/bootstrapping/executable_intellij.sh
@@ -0,0 +1,26 @@
+source "$(dirname $0)/_utility.sh"
+
+# limit this to desktop environments
+exit_if_not_desktop
+
+exit_if_installed idea
+
+set -e
+
+
+if [ "$OS" = "fedora" ]; then
+  # enable flatpak
+  sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+  
+  # install slack
+  sudo flatpak install -y flathub com.slack.Slack
+  sudo flatpak install -y flathub com.jetbrains.IntelliJ-IDEA-Ultimate
+
+  # enable idea command
+
+  echo todo figure out idea command
+  # sudo mkdir -p /app/extra/idea-IU/bin
+  # sudo ln -s /var/lib/flatpak/app/com.jetbrains.IntelliJ-IDEA-Ultimate/x86_64/stable/active/files/extra/idea-IU/bin/idea.sh /app/extra/idea-IU/bin/idea.sh
+  # add alias to .bashrc - alias idea="/var/lib/flatpak/app/com.jetbrains.IntelliJ-IDEA-Ultimate/x86_64/stable/active/files/bin/idea &"
+fi
+
