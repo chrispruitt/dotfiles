cc1f7e3c36a8464ea6c96ba994ec88f3dbc09c7a
Chris Pruitt <chris.pruitt15@gmail.com>
2023-03-20 23:51:55 -0400

    hishtory

---
+++ dot_local/share/bootstrapping/executable_2_hishtory.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+source "$(dirname $0)/_utility.sh"
+exit_if_installed hishtory
+
+set -e
+
+# install the binary
+VERSION=$(curl -sfL https://api.github.com/repos/ddworken/hishtory/releases/latest | jq -r '.tag_name' | sed 's/v//g')
+KERNEL=$(uname -s)
+MACHINE=$(uname -m)
+DOWNLOAD_URL="https://github.com/ddworken/hishtory/releases/download/v${VERSION}/hishtory-${KERNEL}-amd64"
+
+curl -sfL $DOWNLOAD_URL -o $HOME/.local/bin/hishtory
+chmod +x $HOME/.local/bin/hishtory
+
+# install the zsh hooks
+(
+  cd /tmp/
+  curl -sfLo - https://github.com/ddworken/hishtory/archive/refs/tags/v${VERSION}.tar.gz | tar -xzf - hishtory-${VERSION}/client/lib/config.zsh
+  mv hishtory-${VERSION}/client/lib/config.zsh ~/.hishtory/
+
+)
+
+# TODO: init hishtory by setting your server and use your existing hishtory secret
+# HISHTORY_SERVER=https://hishtory.cpru.net hishtory init $(gopass -o cpru/hishtory_secret)
+
+# add the following to your zshrc
+# eexport HISHTORY_SERVER=https://hishtory.cpru.net
+# source /home/cpruitt/.hishtory/config.zsh
+
+
