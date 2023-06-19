541ed3832abd9d26bb2b6d8554c09a2a06045aa2
Chris Pruitt <chris.pruitt15@gmail.com>
2023-06-19 13:22:37 -0400

    bump nomad version

--- dot_local/share/bootstrapping/executable_2_nomad.sh
+++ dot_local/share/bootstrapping/executable_2_nomad.sh
@@ -5,5 +5,5 @@ exit_if_installed nomad
 
 set -e
 
-curl -sfLo - https://releases.hashicorp.com/nomad/1.5.2/nomad_1.5.2_linux_amd64.zip | busybox unzip -qd ~/.local/bin -
+curl -sfLo - https://releases.hashicorp.com/nomad/1.5.6/nomad_1.5.6_linux_amd64.zip | busybox unzip -qd ~/.local/bin -
 chmod +x ~/.local/bin/nomad
